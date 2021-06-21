4f5b7139a976b87d22c0929e37d948321f742a79
Richard Hart <richardhart92@gmail.com>
2021-06-21 12:55:44 -0400

    Wip

---
+++ node.rs
@@ -0,0 +1,26 @@
+
+struct NodeImpl {
+   // r tree 
+}
+
+
+impl NodeImpl for Node {
+
+    fn addEntities(zone: i32, realm: i32, entities: Vec<entity::Entity>) {
+
+    }
+
+    fn removeEntities(entityIds: Vec<i32>) {
+    
+    }
+
+    fn getEntity(id: i32) {
+
+    }
+    
+    fn newQuery() {
+
+    }
+
+}
+
---
+++ poser.rs
@@ -0,0 +1,23 @@
+mod entity;
+
+trait Node {
+
+    fn addEntities(zone: i32, realm: i32, entities: Vec<entity::Entity>);
+    fn removeEntities(entityIds: Vec<i32>);
+    fn getEntity(id: i32);
+    fn newQuery() -> EntityQuery;
+}
+
+trait EntityQuery {
+    fn filterByZone(zones: Vec<i32>) -> Self;
+    fn filterByRealm(realms: Vec<i32>) -> Self;
+    fn filterByBound(bound: ZoneBound) -> Self;
+    fn limit(limit: i32) -> Self;
+    fn offset(offset: i32) -> Self;
+    fn getEntities() -> Vec<entity::Entity>;
+}
+
+trait ZoneBound {
+    fn containsPoint(position: Vec<f32>) -> bool;
+    fn intersect(bound: Self) -> Self;
+}
