482015814cefe8a9cfa3e37f16e42998ff423f73
Richard Hart <richardhart92@gmail.com>
2021-06-21 11:40:52 -0400

    initial commit

---
+++ entity.rs
@@ -0,0 +1,3 @@
+pub struct Entity {
+    pub  position: Vec<f32>
+}
---
+++ main.rs
@@ -0,0 +1,10 @@
+mod entity;
+
+fn main() {
+    let mut e = entity::Entity { position: vec![0.0, 1.0, 2.0, 3.0]};
+
+    // Statements here are executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+}
