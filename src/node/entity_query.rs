0d101220c61a7a10033b30d2494e84f70cb2538c
Richard Hart <richardhart92@gmail.com>
2021-07-11 13:30:34 -0400

    get many impl

--- src/node/entity_query.rs
+++ src/node/entity_query.rs
@@ -19,13 +19,28 @@ mod tests {
         let e2 = entity::Entity { id: 2, x_coordinate: 2.0, y_coordinate: 2.0, z_coordinate: 3.0 };
         let e3 = entity::Entity { id: 3, x_coordinate: 2.0, y_coordinate: 2.0, z_coordinate: 3.0 };
 
-
         node.add_entities(vec![e1, e2, e3]);
         
         let query = node.new_query();
         let entities = node.execute_query(query); 
 
-        assert_eq!(*entities[0], e1);
+        assert_contains_entities(entities, vec![e1, e2, e3]);
+    }
+
+    fn assert_contains_entities(entities: Vec<&entity::Entity>, containees: Vec<entity::Entity>) {
+        for containee in containees.iter() {
+            assert_contains_entity(&entities, containee);
+        }
+    }
+
+    fn assert_contains_entity(entities: &Vec<&entity::Entity>, containee: &entity::Entity) {
+        for entity in entities.iter() {
+            if **entity == *containee {
+                return;
+            }
+        }
+
+        assert!(false, "Entity was not found in the vec");
     }
 }
 
